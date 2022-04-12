9a18d3933a3cf61a18a212a46c508b7244f18658
srabonipro <sraboni391@gmail.com>
2022-04-12 07:36:23 +0600

    function: find even or odd number

---
+++ 8.c
@@ -0,0 +1,24 @@
+// Even or Odd number
+
+#include <stdio.h>
+int isEven(int num);
+
+int main()
+{
+    int num;
+
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+    if(isEven(num))
+        printf("The number is even");
+    else
+        printf("The number is odd");
+
+    return 0;
+}
+
+int isEven(int num)
+{
+    return !(num & 1);
+}
